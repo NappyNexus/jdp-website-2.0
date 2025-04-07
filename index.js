b21688dd70987735357d7d813b050f75c8213925
NappyNexus <gabrielsoriano172@gmail.com>
2025-04-06 20:10:58 -0400

    added fonts and js file

---
+++ index.js
@@ -0,0 +1,12 @@
+{/* <script>
+  const mobileMenuButton = document.getElementById('mobile-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const openIcon = mobileMenuButton.querySelector('svg:first-child');
+  const closeIcon = mobileMenuButton.querySelector('svg:last-child');
+
+  mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+    openIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+  });
+</script> */}
